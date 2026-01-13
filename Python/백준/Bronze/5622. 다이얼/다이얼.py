dec050d0d316451d3a18bed3e22c3999d1d759f3
Seunghwan Yoo <seunghwan081@gmail.com>
2026-01-13 14:02:32 +0900

    [Bronze II] Title: 다이얼, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/5622. 다이얼/다이얼.py
@@ -0,0 +1,7 @@
+s = input()
+
+alphabets =[3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 10, 10, 10, 10]
+time = 0
+for i in range(len(s)):
+    time += alphabets[ord(s[i])-ord("A")]
+print(time)
